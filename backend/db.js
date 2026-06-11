b22f203b038586ef8801bcc766d26888f9bbf66a
Nakul <nakul@Nakuls-MacBook-Air.local>
2026-06-11 12:32:12 +0530

    Day8 Done - Small changes in DB

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURL = process.env.DB_URL;
+// const localMongoDb = 'mongodb://127.0.0.1:27017/signed_app';
 
 const connectDB = async()=>{
     try{
-        const dbURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/signed_app';
+        const dbURI = process.env.MONGO_URI || mongoURL;
         const conn = await mongoose.connect(dbURI);
         console.log(`MongoDB Connected Successfully: ${conn.connection.host}`);
     }catch(error){
